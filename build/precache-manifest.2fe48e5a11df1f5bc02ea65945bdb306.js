d41663ded67465f1b42645c3617f6aa72802043e
metrnametr <patronius1515@gamil.com>
2019-05-28 18:51:17 +0300

    initial commit

---
+++ build/precache-manifest.2fe48e5a11df1f5bc02ea65945bdb306.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3216711f1796e6ed7076580901979758",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e29ee594ee056277bbbc",
+    "url": "/static/css/main.78863de6.chunk.css"
+  },
+  {
+    "revision": "f9f81f69e1c846e78621",
+    "url": "/static/js/2.770eb758.chunk.js"
+  },
+  {
+    "revision": "e29ee594ee056277bbbc",
+    "url": "/static/js/main.7be3f660.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
