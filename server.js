15a4f51f715cf4bc1e047a51dcf9bbba2d820f45
metrnametr <patronius1515@gamil.com>
2019-05-28 19:01:18 +0300

    make it better

--- server.js
+++ server.js
@@ -2,8 +2,6 @@ const express = require('express');
 const path = require('path');
 const port = process.env.PORT || 8080;
 const app = express();
-
-app.use(favicon(__dirname + '/build/favicon.ico'));
 app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, 'build')));
 
